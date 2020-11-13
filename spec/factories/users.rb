9afd96a30abdf681972e7352d8be879dd7350a05
zooootech <yzmisng@gmail.com>
2020-11-13 15:12:30 +0900

    FactoryBotの記述を編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {Faker::Japanese::Name.last_name}
+    first_name            {Faker::Japanese::Name.first_name}
+    last_name_kana        {last_name.yomi}
+    first_name_kana       {first_name.yomi}
+    birthday              {Faker::Date.birthday}
   end
 end
