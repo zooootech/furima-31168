531d18069f3894805e15d4f3c41de93b03e41180
zooootech <yzmisng@gmail.com>
2020-11-15 00:53:01 +0900

    バリデーションの修正
    with_optionメソッドにより、複数のカラムに対して指定のバリデーションを付与
    formatオプションにより、文字列の形式と格納されるエラーメッセージを指定
    allow_blank: trueにより、値が空の場合はバリデーションを実行されないよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+  end
   
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角かな/カナ/漢字を使用してください' }, allow_blank: true do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  with_options format: { with: /\A[ァ-ン]+\z/, message: 'には全角カナを使用してください' }, allow_blank: true do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
