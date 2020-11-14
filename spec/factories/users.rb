a47fbc7893517a19784b1696f02a9d19231edb1b
zooootech <yzmisng@gmail.com>
2020-11-15 00:32:05 +0900

    FactoryBotの修正
    Fakerでつくるダミーデータのパスワードに、必ず数字とアルファベットが混在するようにした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6)+"3a"}
     password_confirmation {password}
     last_name             {Faker::Japanese::Name.last_name}
     first_name            {Faker::Japanese::Name.first_name}
