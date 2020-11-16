d822e203cfe1e888f72f5e6d4227ed8dfb140b28
zooootech <yzmisng@gmail.com>
2020-11-16 20:13:58 +0900

    Itemモデルの編集
    moduleの取り込み
    ActiveHashを用いたアソシエーションの設定
    画像ファイルの紐付け
    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :text
+    validates :image
+  end
+
+  with_options numericality: { greater_than_or_equal_to: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
 end
