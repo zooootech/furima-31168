0337fa8784d2627743a6413365acdba9ce530e00
zooootech <yzmisng@gmail.com>
2020-11-16 18:30:27 +0900

    DaysToShipモデルの編集

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,2 +1,10 @@
-class DaysToShip < ApplicationRecord
+class DaysToShip < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '1~2日で発送' }
+    { id: 2, name: '2~3日で発送' }
+    { id: 3, name: '4~7日で発送' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
 end
