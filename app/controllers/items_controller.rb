1109fb8d8b368c078224eab485c656cafa53fb3f
zooootech <yzmisng@gmail.com>
2020-11-17 19:18:47 +0900

    itemsコントローラーの編集
    ログイン状態によって表示するページを切り替える
    ストロングパラメーターの設定
    出品する際の条件分岐（保存できたとき、保存できなかったとき）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+      
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :price, :text, :image, :category_id, :condition_id, :shipping_charge_id, :days_to_ship_id, :prefecture_id
+    ).merge(user_id: current_user.id)
   end
 end
