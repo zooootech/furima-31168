f27a9e7ebd8df3a044437418e29ccf87741dd247
zooootech <yzmisng@gmail.com>
2020-11-18 18:18:23 +0900

    テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品の出品がうまくいくとき' do
+      it 'image、name、price、text、category_id、condition_id、shipping_chage_id、prefecture_id、days_to_ship_idが存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+      it 'nameが40文字以内であれば出品できる' do
+        @item.name = 'あ' * 40
+        expect(@item).to be_valid
+      end
+      it 'textが1000文字以内であれば出品できる' do
+        @item.text = 'あ' * 1000
+        expect(@item).to be_valid
+      end
+      it 'priceが¥300~9,999,999であれば出品できる' do
+        @item.price = 70000
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品の出品がうまくいかないとき' do
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'textが空だと出品できない' do
+        @item.text = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'category_idに数字が入っていないと出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      it 'condition_idに数字が入っていないと出品できない' do
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition is not a number")
+      end
+      it 'shipping_charge_idに数字が入っていないと出品できない' do
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge is not a number")
+      end
+      it 'prefecture_idに数字が入っていないと出品できない' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it 'days_to_ship_idに数字が入っていないと出品できない' do
+        @item.days_to_ship_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship is not a number")
+      end
+      it 'nameが41文字以上だと出品できない' do
+        @item.name = 'あ' * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
+      it 'textが1001文字以上だと出品できない' do
+        @item.text = 'あ' * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text is too long (maximum is 1000 characters)")
+      end
+      it 'priceが¥300未満だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが¥10,000,000以上だと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'userが紐付いていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
