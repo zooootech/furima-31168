0c9f015f4c1d0740870900de147a03fc0113a992
zooootech <yzmisng@gmail.com>
2020-11-19 15:43:34 +0900

    FactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
     name               { Faker::Name.name }
-    price              { "1000" }
+    price              { 1000 }
     text               { Faker::Lorem.sentence }
-    category_id        { "1" }
-    condition_id       { "1" }
-    shipping_charge_id { "1" }
-    prefecture_id      { "1" }
-    days_to_ship_id    { "1" }
+    category_id        { 1 }
+    condition_id       { 1 }
+    shipping_charge_id { 1 }
+    prefecture_id      { 1 }
+    days_to_ship_id    { 1 }
     association :user
 
     after(:build) do |item|
