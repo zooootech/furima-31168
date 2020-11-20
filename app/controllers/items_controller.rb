8f0b661aae1795cff2beefadd8c376917b28271f
zooootech <yzmisng@gmail.com>
2020-11-20 20:15:46 +0900

    showアクションを設定
    ログインしていないユーザーでも商品詳細ページを閲覧できるよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -10,15 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
     @item = Item.new(item_params)
     if @item.valid?
       @item.save
       redirect_to root_path
     else
       render :new
-      
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
