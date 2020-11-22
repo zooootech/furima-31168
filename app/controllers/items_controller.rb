11398fadfd7d9d1d7adcfdcf79f7d8f8cc430adf
zooootech <yzmisng@gmail.com>
2020-11-22 17:18:23 +0900

    削除ができたらトップページに遷移するよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -37,6 +37,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
