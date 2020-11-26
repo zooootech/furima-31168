26b4f6a8d8ba79f2538474c52da2809d611b4cb1
zooootech <yzmisng@gmail.com>
2020-11-27 02:55:24 +0900

    FactoryBotの編集

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    token { 'tok_abcdefghijk00000000000000000' }
+    postcode { '123-4567' }
+    city { '福岡市' }
+    block { '中央区' }
+    building { '福岡ハイツ' }
+    phone_number { '09012345678' }
+    user_id { 1 }
+    item_id { 1 }
+    prefecture_id { 1 }
   end
 end
