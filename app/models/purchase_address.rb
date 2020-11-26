8111b04ac67676454b103d7048775af1f369ee86
zooootech <yzmisng@gmail.com>
2020-11-26 21:05:03 +0900

    PurchaseAddressクラスのFormオブジェクトの編集
    token属性の追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,10 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor  :hoge, :user_id, :item_id, :postcode, :city, :block, :building, :phone_number, :prefecture_id
+  attr_accessor  :user_id, :item_id, :postcode, :city, :block, :building, :phone_number, :prefecture_id, :token
 
   VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
   VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}\z/
-  
+
   with_options presence: true do
     validates :user_id
     validates :item_id
