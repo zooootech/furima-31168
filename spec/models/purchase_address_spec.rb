5d11af374880e96d8c2c3dc611ff4419c97152eb
zooootech <yzmisng@gmail.com>
2020-11-27 02:55:50 +0900

    テストコードを記述

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase_address = FactoryBot.build(:purchase_address)
+  end
+
+  describe '商品購入機能' do
+    context '商品が購入できるとき' do
+      it 'すべての値が正しく入力されていれば購入できる' do
+        expect(@purchase_address).to be_valid
+      end
+      it 'buildingは空でも購入できる' do
+        @purchase_address.building = ''
+        expect(@purchase_address).to be_valid
+      end
+      it 'phone_numberが10桁の数字だと購入できる' do
+        @purchase_address.phone_number = '0123456789'
+        expect(@purchase_address).to be_valid
+      end
+      it 'phone_numberが11桁の数字だと購入できる' do
+        @purchase_address.phone_number = '01234567890'
+        expect(@purchase_address).to be_valid
+      end
+    end
+
+    context '商品が購入できないとき' do
+      it 'tokenが空では購入できない' do
+        @purchase_address.token = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'postcodeが空では購入できない' do
+        @purchase_address.postcode = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postcode can't be blank")
+      end
+      it 'postcodeにハイフン「-」がないと購入できない' do
+        @purchase_address.postcode = '1234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postcode is invalid")
+      end
+      it 'cityが空では購入できない' do
+        @purchase_address.city = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'blockが空では購入できない' do
+        @purchase_address.block = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Block can't be blank")
+      end
+      it 'phone_numberが空では購入できない' do
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが10桁もしくは11桁の数字以外だと購入できない' do
+        @purchase_address.phone_number = '090123456789'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'phone_numberにハイフンが含まれると購入できない' do
+        @purchase_address.phone_number = '090-123-456'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+    end
+  end
 end
