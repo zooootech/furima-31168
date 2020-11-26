e191049b681e6167093cfddbff6abd4372a25883
zooootech <yzmisng@gmail.com>
2020-11-27 02:49:51 +0900

    PurchaseAddressクラスのFormオブジェクトの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,7 +2,7 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor  :user_id, :item_id, :postcode, :city, :block, :building, :phone_number, :prefecture_id, :token
 
-  VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  VALID_PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
   VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}\z/
 
   with_options presence: true do
