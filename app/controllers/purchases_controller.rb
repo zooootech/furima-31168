78aa2b3ad8b43d6dc8f47215698b76d313c55c00
zooootech <yzmisng@gmail.com>
2020-11-26 21:20:44 +0900

    purchasesコントローラーの編集
    決済機能のサーバーサイド実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -13,6 +13,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -24,5 +25,14 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:postcode, :city, :block, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_2e7fe3c9660bd051ce9c9407"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
